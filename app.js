e80e48226dcadb1282f0b42934c3c56bd7875a87
Syed Suhail <suhailpno@gmail.com>
2025-03-01 23:01:17 +0530

    Update app.js

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ const app = express();
 // Middleware
 app.use(express.json());
 app.use(cors({
-  origin: 'http://localhost:3000',
+  origin: 'https://zen-clients.netlify.app/',
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization']
