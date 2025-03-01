24d6f316d6bd09bbda5c149779d1b966e4863cd2
Syed Suhail <suhailpno@gmail.com>
2025-03-01 23:13:58 +0530

    Update server.js

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const PORT = process.env.PORT || 5000;
 
 // CORS configuration
 app.use(cors({
-  origin: ['http://localhost:3000'],
+  origin: ['https://zen-clients.netlify.app'],
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   credentials: true,
   allowedHeaders: ['Content-Type', 'Authorization']
